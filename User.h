312c8e8d1aae3982d025d15621ab2cc7c3121810
cpbarry <Colin@prototypelab2.dhcp.asu.edu>
2015-05-02 12:23:20 -0700

    Do you see this commit?

---
+++ User.h
@@ -0,0 +1,18 @@
+//
+//  User.h
+//  FallApp
+//
+//  Created by Colin Barry on 5/2/15.
+//  Copyright (c) 2015 Julio Morera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface User : NSManagedObject
+
+@property (nonatomic, retain) NSString * firstName;
+@property (nonatomic, retain) NSString * lastName;
+
+@end
---
+++ User.m
@@ -0,0 +1,17 @@
+//
+//  User.m
+//  FallApp
+//
+//  Created by Colin Barry on 5/2/15.
+//  Copyright (c) 2015 Julio Morera. All rights reserved.
+//
+
+#import "User.h"
+
+
+@implementation User
+
+@dynamic firstName;
+@dynamic lastName;
+
+@end
